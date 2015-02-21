74c254b84875a694ea63a352bcb60eaab14e33f6
switypi <subhash62@gmail.com>
2015-02-22 00:27:57 +0530

    Project creation

---
+++ VideoConvertor/VideoConvertor/VideoConvertor/ConvertorEngine.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoConvertor
+{
+    public class ConvertorEngine
+    {
+
+        public ConvertorEngine(string sFilePath, string sOutPutPath, string sfileFormat)
+        {
+            FilePath = sFilePath;
+            FileFormat = sfileFormat;
+            OutPutFilePath = sOutPutPath;
+
+        }
+        public ConvertorEngine()
+        {
+
+        }
+
+        public string Convert()
+        {
+           
+            string smessage = string.Empty;
+
+            if (string.IsNullOrEmpty(FilePath) || string.IsNullOrEmpty(OutPutFilePath) || string.IsNullOrEmpty(FileFormat))
+            {
+                smessage = "Input path,Output path and file format are maindatory.";
+            }
+            else
+            {
+                try
+                {
+                   
+                    var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                    switch(FileFormat)
+                    {
+                        case Format.webm:
+                             ffMpeg.ConvertMedia(FilePath, OutPutFilePath, Format.webm);
+                            break;
+                        case Format.mp4:
+                            ffMpeg.ConvertMedia(FilePath, OutPutFilePath, Format.mp4);
+                            break;
+                        case Format.mov:
+                            ffMpeg.ConvertMedia(FilePath, OutPutFilePath, Format.mov);
+                            break;
+                    }
+                   
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Unable to convert. "+ex.Message);
+                }
+               
+            }
+            return smessage;
+        }
+
+        #region Properties
+        public string FilePath { get; set; }
+
+        public string FileFormat { get; set; }
+
+        public string OutPutFilePath { get; set; }
+
+       
+        #endregion
+
+    }
+}
---
+++ VideoConvertor/VideoConvertor/VideoConvertor/Format.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoConvertor
+{
+    public class Format
+    {
+
+        public const string webm = "webm";
+
+      
+        public const string mov = "mov";
+        
+        public const string mp4 = "mp4";
+    }
+
+
+}
